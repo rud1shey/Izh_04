e0966555d96bdb14da7b1232750d0d8604f40b7e
rud1shey <shiryayevdmitry@gmail.com>
2019-10-03 18:06:39 +0400

    create project izh_04

---
+++ Izh_04_cl/Program.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Izh_04_cl
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+        }
+
+        public static int FindMaxValue(int[] array)
+        {
+            int result = 0;
+            foreach (int item in array)
+            {
+                if (item > result)
+                {
+                    result = item;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ izh_04/Program.cs
@@ -0,0 +1,30 @@
+﻿// <copyright file="Program.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace Izh_04
+{
+    using System;
+
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int FindMaxElement(int[] array)
+        {
+            int result = 0;
+            foreach (int item in array)
+            {
+                if (result < item)
+                {
+                    result = item;
+                }
+            }
+
+            return result;
+        }
+    }
+}
